87bedf39053633d72fb4b77e8b03a02f659d16c0
Ben Willis <benjamin.willis@gmail.com>
2012-10-14 01:06:28 -0700

    fixing seeds file and changing attr_accessible released_on to released_at to make store-after run

--- store-after/app/models/product.rb
+++ store-after/app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ActiveRecord::Base
   belongs_to :category
-  attr_accessible :name, :price, :released_on, :category, :category_id
+  attr_accessible :name, :price, :released_at, :category, :category_id
 
   def released_on # quick hack to get the view working
     released_at.to_date
